b6627c105e0e8db2966acb81e9014119a7eb3242
Ian Dennis Miller <ian@iandennismiller.com>
2013-12-13 09:57:33 -0500

    separate journal file for phone and tablet

--- bin/journal_rotate.py
+++ bin/journal_rotate.py
@@ -18,12 +18,14 @@ dropbox_path = "/Users/idm/Dropbox/journal"
 
 http_file = os.path.join(journal_path, 'http/latest.txt')
 live_file = os.path.join(journal_path, 'journal.txt')
-dropbox_file = os.path.join(dropbox_path, 'journal.txt')
+dropbox_phone_file = os.path.join(dropbox_path, 'journal-phone.txt')
+dropbox_tablet_file = os.path.join(dropbox_path, 'journal-tablet.txt')
 
 # create backups
 
 os.system("cp %s %s/%s-journal.txt" % (live_file, backup_path, datestamp))
-os.system("cp %s %s/%s-dropbox.txt" % (dropbox_file, backup_path, datestamp))
+os.system("cp %s %s/%s-dropbox-phone.txt" % (dropbox_phone_file, backup_path, datestamp))
+os.system("cp %s %s/%s-dropbox-tablet.txt" % (dropbox_tablet_file, backup_path, datestamp))
 
 # download HTTP and purge/rotate
 
@@ -40,17 +42,18 @@ app.logger.debug("start rotating journal")
 j = GT.journal.Journal(export_path)
 j.parse(live_file)
 j.parse(http_file)
-j.parse(dropbox_file)
+j.parse(dropbox_phone_file)
+j.parse(dropbox_tablet_file)
 
 exported = j.export_week_old()
-if exported:
-    j.purge_week_old(live_file)
+j.purge_week_old(live_file)
 
 app.logger.debug("finish rotating journal")
 
 # rotate out Dropbox file
 
 app.logger.debug("clear dropbox journal.txt")
-os.system("echo '' > %s" % dropbox_file)
+os.system("echo -n '' > %s" % dropbox_phone_file)
+os.system("echo -n '' > %s" % dropbox_tablet_file)
 
 app.logger.debug("DONE")
