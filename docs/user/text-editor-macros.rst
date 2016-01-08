daa57f5f3fdab68e6a815bff5f202dce9a087c09
Ian Dennis Miller <ian@iandennismiller.com>
2016-01-08 00:08:45 -0500

    working on sublime text

--- docs/user/text-editor-macros.rst
+++ docs/user/text-editor-macros.rst
@@ -1,2 +1,25 @@
 Text Editor Macros
 ==================
+
+It would be pretty annoying to manually type out a timestamp every time you wanted to make a journal entry.  Luckily, many text editors can be extended with macros to make this automatic.
+
+Sublime Text
+------------
+
+Download 
+
+
+Add the following to ``Default (OSX).sublime-keymap``
+
+::
+
+    [
+        { "keys": ["ctrl+super+alt+n"], "command": "insert_date" },
+        { "keys": ["ctrl+super+alt+m"], "command": "insert_time" }
+    ]
+
+
+
+Jota+ for Android
+-----------------
+
---
+++ integration/sublime-text/journal_date.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# gthnk (c) 2014-2016 Ian Dennis Miller
+
+import sublime
+import sublime_plugin
+import datetime
+
+
+def scroll_to_end(self):
+    (row, col) = self.view.rowcol(1000000)
+    pt = self.view.text_point(row, 0)
+
+    self.view.sel().clear()
+    self.view.sel().add(sublime.Region(pt))
+    self.view.show(pt)
+
+
+class insert_date(sublime_plugin.TextCommand):
+    """Add the next occurrence of the word under the cursor to the selection"""
+
+    def run(self, edit):
+        scroll_to_end(self)
+        n = datetime.datetime.now()
+        stamp = "%s-%0.2d-%0.2d\n\n" % (n.year, int(n.month), int(n.day))
+        self.view.insert(edit, self.view.sel()[0].end(), stamp)
+
+
+class insert_time(sublime_plugin.TextCommand):
+    """Add the next occurrence of the word under the cursor to the selection"""
+
+    def run(self, edit):
+        scroll_to_end(self)
+
+        n = datetime.datetime.now()
+        stamp = "%0.2d%0.2d\n\n" % (int(n.hour), int(n.minute))
+        self.view.insert(edit, self.view.sel()[0].end(), stamp)
+        self.view.run_command("save")
