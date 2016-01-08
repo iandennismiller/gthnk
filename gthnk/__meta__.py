05801d42e4304bca0022ab9e6c3f2e6853e80f52
Ian Dennis Miller <ian@iandennismiller.com>
2016-01-07 20:08:54 -0500

    do not need lxml

--- gthnk/__meta__.py
+++ gthnk/__meta__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # gthnk (c) 2014-2016 Ian Dennis Miller
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __author__ = 'Ian Dennis Miller'
 __email__ = 'iandennismiller@gmail.com'
 __url__ = 'http://gthnk.com'
