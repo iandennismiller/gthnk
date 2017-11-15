6226fd281af1f7c1600863ca661f589f26fa810a
Ian Dennis Miller <ian@iandennismiller.com>
2017-11-15 15:49:39 -0500

    append

---
+++ gthnk/tests/mixins.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# gthnk (c) 2017 Ian Dennis Miller
+
+from flask import current_app
+from .. import create_app, db
+from flask_testing import TestCase
+
+
+class DiamondTestCase(TestCase):
+    def create_app(self):
+        """
+        Create a Flask-Diamond app for testing.
+        """
+
+        app = create_app()
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        return app
+
+    def setUp(self):
+        """
+        Prepare for a test case.
+        """
+
+        db.create_all()
+        current_app.logger.debug("setup complete")
+
+    def tearDown(self):
+        """
+        Clean up after a test case.
+        """
+
+        db.session.remove()
+        db.drop_all()
