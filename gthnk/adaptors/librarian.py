a543cb58f33c280d8411265034627d54bfb1b5ec
Ian Dennis Miller <ian@iandennismiller.com>
2020-01-02 15:22:01 -0500

    encoding problems...

--- gthnk/adaptors/librarian.py
+++ gthnk/adaptors/librarian.py
@@ -15,7 +15,7 @@ def overwrite_if_different(filename, new_content):
     if os.path.isfile(filename):
         # if so, gather the md5 checksums
         with open(filename, "r", encoding='utf-8') as f:
-            existing_checksum = hashlib.md5(f.read()).hexdigest()
+            existing_checksum = hashlib.md5(f.read().encode('utf-8')).hexdigest()
         generated_checksum = hashlib.md5(new_content).hexdigest()
 
         # compare to md5 checksum of generated file.
