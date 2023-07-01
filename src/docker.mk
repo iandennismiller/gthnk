ff9f571138e5b13f6ea9bf71df0da53eb876895d
Ian Dennis Miller <ian@iandennismiller.com>
2023-07-01 18:05:00 -0400

    update to 0.8.1 push new image to docker hub

--- src/docker.mk
+++ src/docker.mk
@@ -1,5 +1,7 @@
 # gthnk
 
+GTHNK_VER=$(word 3, $(shell grep __version__ src/gthnk/__meta__.py))
+
 DOCKER_CMD=docker run \
 	-it \
 	--rm \
@@ -7,7 +9,7 @@ DOCKER_CMD=docker run \
 	-p 1620:1620 \
 	-e TZ=America/Toronto \
 	-v ~/Work/gthnk/var/gthnk:/opt/gthnk/var \
-	iandennismiller/gthnk:0.8
+	iandennismiller/gthnk:$(GTHNK_VER)
 
 docker-shell:
 	cd src && $(DOCKER_CMD) /bin/bash
@@ -16,7 +18,7 @@ docker-server:
 	cd src && $(DOCKER_CMD)
 
 docker-build: clean
-	cd src && docker build -t iandennismiller/gthnk:0.8 .
+	cd src && docker build -t iandennismiller/gthnk:$(GTHNK_VER) .
 
 docker-push:
-	cd src && docker buildx build --platform linux/amd64,linux/arm64 -t iandennismiller/gthnk:0.8 --push .
+	cd src && docker buildx build --platform linux/amd64,linux/arm64 -t iandennismiller/gthnk:$(GTHNK_VER) --push .
--- src/gthnk/__meta__.py
+++ src/gthnk/__meta__.py
@@ -1,6 +1,6 @@
 # gthnk (c) Ian Dennis Miller
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 __author__ = 'Ian Dennis Miller'
 __email__ = 'ian@iandennismiller.com'
 __url__ = 'http://www.gthnk.com'
